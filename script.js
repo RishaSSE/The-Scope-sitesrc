d4d374280f91da40d6a3aee8c4d31309f96e0a63
Rishaan Sharma <rishaan.rs.sharma@gmail.com>
2025-08-21 23:04:44 -0700

    Improved design, scrolling animation for navbar, new background, still issue w/ scrollbar bottom having a white rectangle

--- script.js
+++ script.js
@@ -2,10 +2,28 @@ var lFollowX = 0,
     lFollowY = 0,
     x = 0,
     y = 0,
-    friction = 1 / 30;
+    friction = 1 / 200;
 const browserWidth = window.innerWidth;
 const browserHeight = window.innerHeight;
+$(document).ready(function () {
+  $(".wrap").on('scroll', function () {
+    if ($(this).scrollTop() > 0) {
+      $('#navbar').css('padding', '5px');
+      $('#site-title').css('height', '0px');
+      $('#site-title').css('width', '0px');
+      $('#site-title').css('opacity', '0%');
+      $('#navbar').css('box-shadow', '0px 2px 10px');
 
+    } else {
+      $('#navbar').css('padding', '15px');
+      $('#site-title').css('height', '100px');
+      $('#site-title').css('width', '40px');
+      $('#site-title').css('opacity', '100%');
+      $('#navbar').css('box-shadow', '0px 0px 0px');
+
+    }
+  });
+});
 function moveBackground() {
   x += (lFollowX - x) * friction;
   y += (lFollowY - y) * friction;
@@ -25,8 +43,8 @@ $(window).on('mousemove', function(e) {
 
   var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
   var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
-  lFollowX = (10 * lMouseX) / 100;
-  lFollowY = (10 * lMouseY) / 100;
+  lFollowX = (15 * lMouseX) / 100;
+  lFollowY = (15 * lMouseY) / 100;
 
 });
 
